a0db210f0525a6ae95e1ec2cf7c16d7b2661d8a1
irwansiswandy <irwansiswandymks@gmail.com>
2019-04-24 15:59:27 +0700

    2019-04-24_15-59

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,6 @@ module.exports = {
             .type("javascript/auto")
             .use("i18n")
             .loader("@kazupon/vue-i18n-loader")
-            .end();
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].meta = { viewport: 'width=device-width,initial-scale=1' }
-            });
-        
+            .end();        
     }
 }
