6c70399531f7723c3915cafa8c69f65662e40cba
irwansiswandy <irwansiswandymks@gmail.com>
2019-11-14 13:49:29 +0700

    2019-11-14_13-49

--- src/_variables.js
+++ src/_variables.js
@@ -1,9 +1,17 @@
 window._ = require('lodash');
 
 const ServerHostname = 'http://localhost:81/';
+// const ServerHostname = 'http://primasakti-network.ddns.net:281';
+
 const BaseApiURL = ServerHostname + 'api/';
 const PUSHER_SETTINGS = {
-    key: '937c40c45b8b04b84c49',
+    // irwansiswandymks@gmail.com
+    // key: '937c40c45b8b04b84c49',
+    // cluster: 'ap1',
+    // channel: 'primasakti-user'
+
+    // primasakti1.sby@gmail.com
+    key: '9b0508a6e839d5d65234',
     cluster: 'ap1',
     channel: 'primasakti-user'
 };
